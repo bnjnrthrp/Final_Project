1ad6103b8451d419b01436759b45d796ba742369
Benji Northrop <benji.northrop@gmail.com>
2024-04-08 21:49:04 -0700

    Built the GUI

--- src/main/java/JFrameView.java
+++ src/main/java/JFrameView.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.BoxLayout;
@@ -7,102 +9,119 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.lang.StringBuilder;
+import javax.swing.ScrollPaneLayout;
 
 public class JFrameView<T> extends JFrame implements IView<T>{
-  private JLabel display, accountDisplay, toolDisplay, jigDisplay, woodDisplay, furnitureDisplay;
+  private JLabel display, accountDisplay;
+  private JTextArea toolDisplay, jigDisplay, woodDisplay, furnitureDisplay;;
   private JButton exitButton, goToWork, buyWood, buyDimensional, buyTool, buyJig, cutWood, makeFurniture;
-  private JTextField inputTool, inputWood, inputJig, inputFurniture;
+  private JTextField inputTool, inputWood, inputJig, inputFurniture, inputDimension;
 
   public JFrameView(String caption){
     super(caption);
 
-    setSize(500, 300);
+    setSize(1200, 800);
     setLocation(200, 200);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     JPanel accountPanel = new JPanel();
+    JPanel toolDisplayPanel = new JPanel();
+    JPanel jigDisplayPanel = new JPanel();
+    JPanel woodDisplayPanel = new JPanel();
     JPanel storePanel = new JPanel();
-    JPanel toolPanel = new JPanel();
-    JPanel jigPanel = new JPanel();
+    JPanel buyToolPanel = new JPanel();
+    JPanel buyJigPanel = new JPanel();
     JPanel workshopPanel = new JPanel();
+    JPanel furnitureDisplayPanel = new JPanel();
+    JPanel makeFurniturePanel = new JPanel();
+    JPanel createdFurniturePanel = new JPanel();
     JPanel exitPanel = new JPanel();
 
+    this.setLayout(new GridLayout(3, 4, 0, 0));
 
-//    this.setLayout(new GridLayout(5, 3));
-
+//    accountPanel.setLayout(new GridLayout(3, 0));
+    // Add to account panel
     display = new JLabel("Welcome to the Woodshop");
-    accountDisplay = new JLabel("Account Balance");
-    toolDisplay = new JLabel("Tool Inventory:");
-    jigDisplay = new JLabel("Jig Inventory");
-    woodDisplay = new JLabel("Wood Inventory");
-    furnitureDisplay = new JLabel("Furniture available to Make");
     accountPanel.add(display);
+    accountDisplay = new JLabel("Account Balance: ");
     accountPanel.add(accountDisplay);
-//    this.add(display);
-//    this.add(accountDisplay);
-    toolPanel.add(toolDisplay);
-    jigPanel.add(jigDisplay);
     //go to work button
-
     goToWork = new JButton("Go to Work");
     goToWork.setActionCommand("goToWork");
     accountPanel.add(goToWork);
 
-    //buy plywood
+    // Add to tool panel
+    toolDisplay = new JTextArea("Tool Inventory:");
+    toolDisplay.setLineWrap(true);
+    toolDisplayPanel.add(toolDisplay);
+
+    // Add jig display
+    jigDisplay = new JTextArea("Jig Inventory");
+    jigDisplayPanel.add(jigDisplay);
+
+    // Add wood display
+    woodDisplay = new JTextArea("Wood Inventory");
+    woodDisplayPanel.add(woodDisplay);
+
+
+    // Add store interface - ply and idmensional
     buyWood = new JButton("Buy Plywood");
     buyWood.setActionCommand("buyPlywood");
     storePanel.add(buyWood);
-
-    inputWood = new JTextField(2);
-    storePanel.add(inputWood);
-
-
     //buy dimensional
     buyDimensional = new JButton("Buy dimensional (2x4)");
     buyDimensional.setActionCommand("buyDimensional");
     storePanel.add(buyDimensional);
+    storePanel.setLayout(new BoxLayout(storePanel, BoxLayout.Y_AXIS));
 
-    //buy tool
 
-    buyTool = new JButton("Buy tool");
-    buyTool.setActionCommand("buyTool");
-    storePanel.add(buyTool);
-
+    // buy tool
     // input field tool
     inputTool = new JTextField(2);
-    storePanel.add(inputTool);
+    buyToolPanel.add(inputTool);
+    buyTool = new JButton("Buy tool");
+    buyTool.setActionCommand("buyTool");
+    buyToolPanel.add(buyTool);
 
 
-    //buy Jig
-
+    //buy Jig panel
     buyJig = new JButton("Buy jig");
     buyJig.setActionCommand("buyJig");
-    storePanel.add(buyJig);
-
+    buyJigPanel.add(buyJig);
     // input field jig
     inputJig = new JTextField(2);
-    storePanel.add(inputJig);
+    buyJigPanel.add(inputJig);
 
-    this.add(woodDisplay);
+
+    //Use workshop panel
+
+    inputWood = new JTextField(2);
+    workshopPanel.add(inputWood);
+    inputDimension = new JTextField(4);
+    workshopPanel.add(inputDimension);
     cutWood = new JButton("Cut wood");
     cutWood.setActionCommand("cutWood");
     workshopPanel.add(cutWood);
 
-
+    furnitureDisplay = new JTextArea("Furniture available to Make");
+    furnitureDisplayPanel.add(furnitureDisplay);
 
 
     //make furniture
-
-    makeFurniture = new JButton("Make Furniture");
-    makeFurniture.setActionCommand("makeFurniture");
-    workshopPanel.add(makeFurniture);
-
     // input field furniture
     inputFurniture = new JTextField(2);
     workshopPanel.add(inputFurniture);
+    makeFurniture = new JButton("Make Furniture");
+    makeFurniture.setActionCommand("makeFurniture");
+    makeFurniturePanel.add(makeFurniture);
 
+    // made furniture display
+    furnitureDisplay = new JTextArea("Made furniture");
+    furnitureDisplayPanel.add(furnitureDisplay);
+    createdFurniturePanel.add(furnitureDisplay);
 
     //exit button
     exitButton = new JButton("Exit");
@@ -111,13 +130,17 @@ public class JFrameView<T> extends JFrame implements IView<T>{
     workshopPanel.add(furnitureDisplay);
     // pack();
 
-    this.setLayout(new GridLayout(1, 6));
-
     this.add(accountPanel);
+    this.add(toolDisplayPanel);
+    this.add(jigDisplayPanel);
+    this.add(woodDisplayPanel);
     this.add(storePanel);
+    this.add(buyToolPanel);
+    this.add(buyJigPanel);
     this.add(workshopPanel);
-    this.add(toolPanel);
-    this.add(jigPanel);
+    this.add(furnitureDisplayPanel);
+    this.add(makeFurniturePanel);
+    this.add(createdFurniturePanel);
     this.add(exitPanel);
 
     setVisible(true);
@@ -198,10 +221,10 @@ public class JFrameView<T> extends JFrame implements IView<T>{
 
   @Override
   public void showAll(int number, ADTList<Tool> tools, ADTList<Jig> jigs, ADTList<Wood> wood, ADTList<Furniture> furniture) {
-    this.accountDisplay.setText(Integer.toString(number));
-    this.toolDisplay.setText(tools.toString());
-    this.jigDisplay.setText(jigs.toString());
-    this.woodDisplay.setText(wood.toString());
-    this.furnitureDisplay.setText(furniture.toString());
+    this.accountDisplay.setText("Account Balance: " + Integer.toString(number));
+    this.toolDisplay.setText("Tools: \n" + tools);
+    this.jigDisplay.setText("Jigs: \n" + jigs.toString());
+    this.woodDisplay.setText("Wood: \n" + wood.toString());
+    this.furnitureDisplay.setText("Furniture :" + furniture.toString());
     }
 }
